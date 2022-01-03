e61fade99ba84b4c1e0df6f0ca519c980054f603
Mike Barry <michael23barry@gmail.com>
2022-01-02 19:13:03 -0500

    Fixing react app for github

---
+++ react-app/App.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import MyStack from './ScreenStack.js'
+
+export default function App() {
+    return (
+        <MyStack />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
